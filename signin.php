e8855c93e3e0c82c318a77cdd2d94d22f1072601
InaamAmjad <Amjad-I1@ulster.ac.uk>
2025-01-03 21:41:07 +0000

    signin.php

--- signin.php
+++ signin.php
@@ -8,22 +8,23 @@ $servername = "reels-server.mysql.database.azure.com"; // Azure MySQL hostname
 $username = "reelsmydb"; // Your username
 $password = "CO$r2iaiKYUkU7Jv"; // Your password
 $dbname = "reels_db"; // Your database name
-
+ echo "<script>alert('befoersslpoint');</script>";
 // SSL certificate path (you need to download the CA certificate)
 $ca_cert_path = "/home/site/ssl_certs/DigiCertGlobalRootCA.crt.pem"; // Replace with the actual path to the CA certificate
-
+ echo "<script>alert('afterssl');</script>";
 // Create connection
 $conn = mysqli_init();
 
 // Set SSL certificates
 mysqli_ssl_set($conn, NULL, NULL, $ca_cert_path, NULL, NULL);
-
+ echo "<script>alert('setssl');</script>";
 // Connect to MySQL with SSL
 if (!mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL)) {
     die("Connection failed: " . mysqli_connect_error());
 } else {
     echo "<script>alert('connection successful');</script>";
 }
+ echo "<script>alert('endpoint');</script>";
 // Handle form submission
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['signin'])) {
     // Validate that form inputs are set and not empty
