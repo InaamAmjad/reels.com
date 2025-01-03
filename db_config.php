6d5e3004e85eacf4a4d67712bfb223e886915828
InaamAmjad <Amjad-I1@ulster.ac.uk>
2025-01-03 19:00:46 +0000

    db_config.php

--- db_config.php
+++ db_config.php
@@ -11,6 +11,9 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}else {
+    // Successful connection popup
+    echo "<script>alert('Database connection successful!');</script>";
 }
 ?>
  
