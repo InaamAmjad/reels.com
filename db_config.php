3030fdf9bf3ba2814a14cb26e651cc8320692647
InaamAmjad <Amjad-I1@ulster.ac.uk>
2025-01-03 23:15:23 +0000

    db_config.php

--- db_config.php
+++ db_config.php
@@ -1,16 +1,20 @@
 <?php
 // Database connection settings
-$servername = "reels-server.mysql.database.azure.com";
-$username = "reelsmydb";
-$password = "CO$r2iaiKYUkU7Jv";
-$dbname = "reels_db";
-
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
+$serverName = "reels-server.mysql.database.azure.com";  // Full server name
+$connectionOptions = array(
+    "Database" => "reels_db", 
+    "Uid" => "reelsmydb", 
+    "PWD" => "Nomi4321",
+    "Encrypt" => true,                  // Enable SSL encryption
+    "TrustServerCertificate" => false,  // Ensure the server certificate is validated
+);
+$conn = sqlsrv_connect($serverName, $connectionOptions);
 // Check connection
 if ($conn->connect_error) {
+  echo "<script>alert('dying');</script>";
     die("Connection failed: " . $conn->connect_error);
+}else {
+    // Show an alert for a successful connection
+    echo "<script>alert('Connection successful');</script>";
 }
 ?>
