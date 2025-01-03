e3b5fd20f28deafd3b80c67dc1273850f9c21fa0
InaamAmjad <Amjad-I1@ulster.ac.uk>
2025-01-03 20:34:04 +0000

    db_config.php

--- db_config.php
+++ db_config.php
@@ -1,19 +1,25 @@
 <?php
 // Database connection settings
+$servername = "reels-server.mysql.database.azure.com"; // Azure MySQL hostname
+$username = "reelsmydb"; // Your username
+$password = "CO$r2iaiKYUkU7Jv"; // Your password
+$dbname = "reels_db"; // Your database name
 
-$servername = "reels-server.mysql.database.azure.com";
-$username = "reelsmydb";
-$password = "CO$r2iaiKYUkU7Jv";
-$dbname = "reels_db";
+// SSL certificate path (you need to download the CA certificate)
+$ca_cert_path = "/home/site/ssl_certs/DigiCertGlobalRootCA.crt.pem"; // Replace with the actual path to the CA certificate
+
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_init();
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}else {
-    // Successful connection popup
-    echo "<script>alert('Database connection successful!');</script>";
+// Set SSL certificates
+mysqli_ssl_set($conn, NULL, NULL, $ca_cert_path, NULL, NULL);
+
+// Connect to MySQL with SSL
+if (!mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL)) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connection successful!";
 }
 ?>
+
  
