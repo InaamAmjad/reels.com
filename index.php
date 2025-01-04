2b9aa74557c142eb44ef096d4c3cdef252ab15cd
InaamAmjad <Amjad-I1@ulster.ac.uk>
2025-01-04 23:04:30 +0000

    index.php

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
     <div id="video-container">
         <?php
             include('db_config.php');
-            $sql = "SELECT * FROM videos";
+            $sql = "SELECT * FROM reels_db.videos";
             $result = $conn->query($sql);
             if ($result->num_rows > 0) {
                 while($row = $result->fetch_assoc()) {
                     $video_id = $row['id'];
                     // Fetch likes count
-                    $like_sql = "SELECT COUNT(*) AS like_count FROM likes WHERE video_id = $video_id";
+                    $like_sql = "SELECT COUNT(*) AS like_count FROM reels_db.likes WHERE video_id = $video_id";
                     $like_result = $conn->query($like_sql);
                     $like_data = $like_result->fetch_assoc();
                     $likes = $like_data['like_count'];
 
                     // Fetch comments for the video
-                    $comment_sql = "SELECT comment_text FROM comments WHERE video_id = $video_id";
+                    $comment_sql = "SELECT comment_text FROM reels_db.comments WHERE video_id = $video_id";
                     $comment_result = $conn->query($comment_sql);
                     $comments = [];
                     while ($comment = $comment_result->fetch_assoc()) {
