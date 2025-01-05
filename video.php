1847b1d5898c0f32e8b351ced07b2cb625b80af9
InaamAmjad <Amjad-I1@ulster.ac.uk>
2025-01-05 16:33:11 +0000

    video.php

--- video.php
+++ video.php
@@ -2,7 +2,7 @@
 session_start();
 
     if (!isset($_SESSION['role']) || $_SESSION['role'] === 'consumer') {
-    echo "<script>alert('You do not have permission to upload videos: $_SESSION['role'].'); window.location.href='index.php';</script>";
+    echo "<script>alert('You do not have permission to upload videos:" . $_SESSION['role'] . ".'); window.location.href='index.php';</script>";
     exit();
 }
 
