3381e8d15aec018e32f6c61e6e2f903fbc6e334a
InaamAmjad <Amjad-I1@ulster.ac.uk>
2025-01-05 00:18:45 +0000

    Update signup.php

--- signup.php
+++ signup.php
@@ -2,21 +2,8 @@
 session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['signup'])) {
     // Database credentials
-    $servername = "reels-server.mysql.database.azure.com";
-    $username = "reelsmydb";
-    $password = "Nomi4321";
-    $dbname = "reels_db";
-
-    $con = mysqli_init();  // Initialize the MySQL connection
-
     // Set up SSL parameters
-    mysqli_ssl_set($con, NULL, NULL, "/home/site/ssl_certs/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-
-    // Establish a connection to the MySQL database
-    if (!mysqli_real_connect($con, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL)) {
-        // Check connection and handle errors
-        die("Connection failed: " . mysqli_connect_error());
-    }
+       include('db_config.php');
 
     // Sanitize and validate input
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
