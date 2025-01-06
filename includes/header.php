ea486b786a77b525f0c879010b5c9f658943b9be
InaamAmjad <Amjad-I1@ulster.ac.uk>
2025-01-06 19:02:03 +0000

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,8 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,10 +19,14 @@
         <nav>
             <ul>
                 <li><a href="index.php">Home</a></li>
-                <li><a href="signin.php">Sign In</a></li>
-                <li><a href="signup.php">Sign Up</a></li>
-                <li><a href="video.php">Videos</a></li>
+                <?php if (isset($_SESSION['user_id'])): ?>
+                    <li><a href="video.php">Videos</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                <?php else: ?>
+                    <li><a href="signin.php">Sign In</a></li>
+                    <li><a href="signup.php">Sign Up</a></li>
+                <?php endif; ?>
             </ul>
         </nav>
     </header>
- 
+
